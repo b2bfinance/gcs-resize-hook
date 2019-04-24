745d80c8abc21b348e0a126f6bacfa71c6b881c9
Bradley Weston <hello@bweston.me>
2019-04-24 15:31:26 +0100

    Remove members that are not used from GCSEvent struct.

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/disintegration/imaging"
 
@@ -31,12 +30,8 @@ var (
 // GCSEvent is the payload of a GCS event. Please refer to the docs for
 // additional information regarding GCS events.
 type GCSEvent struct {
-	Bucket         string    `json:"bucket"`
-	Name           string    `json:"name"`
-	Metageneration string    `json:"metageneration"`
-	ResourceState  string    `json:"resourceState"`
-	TimeCreated    time.Time `json:"timeCreated"`
-	Updated        time.Time `json:"updated"`
+	Bucket string `json:"bucket"`
+	Name   string `json:"name"`
 }
 
 func initialiseCfg() {
